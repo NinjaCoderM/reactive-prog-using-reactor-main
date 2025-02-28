786dd6333cc4c1a676f905c6d39ae2e56994d2d0
wienm <martin.scheibelreiter@chello.at>
2025-02-28 21:49:37 +0100

    Build Movie ReactiveService StepVerifier.create(...).assertNext( x -> { assertEquals(...)

---
+++ src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceTest.java
@@ -0,0 +1,35 @@
+package com.learnreactiveprogramming.service;
+
+import com.learnreactiveprogramming.domain.Movie;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieReactiveServiceTest {
+    MovieInfoService movieInfoService = new MovieInfoService();
+    ReviewService reviewService = new ReviewService();
+    MovieReactiveService movieReactiveService = new MovieReactiveService(movieInfoService, reviewService);
+
+
+    @Test
+    void getAllMovies() {
+        Flux<Movie> allMovies = movieReactiveService.getAllMovies();
+        allMovies.subscribe(System.out::println);
+        StepVerifier.create(allMovies)
+                .assertNext( movie -> {
+                    assertEquals("Batman Begins", movie.getMovie().getName(), "Name of movie should be Batman Begins");
+                    assertEquals(2, movie.getReviewList().size(), "Expect 2 reviews");
+                })
+                .assertNext( movie -> {
+                    assertEquals("The Dark Knight", movie.getMovie().getName(), "Name of movie should be The Dark Knight");
+                    assertEquals(2, movie.getReviewList().size(), "Expect 2 reviews");
+                })
+                .assertNext( movie -> {
+                    assertEquals("Dark Knight Rises", movie.getMovie().getName(), "Name of movie should be batman begins");
+                    assertEquals(2, movie.getReviewList().size(), "Expect 2 reviews");
+                })
+                .verifyComplete();
+    }
+}
