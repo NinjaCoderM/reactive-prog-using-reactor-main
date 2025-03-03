a6b75d111163b71fbf936e3a0bbc6bc6032c8c0d
wienm <martin.scheibelreiter@chello.at>
2025-03-03 17:29:08 +0100

    Mockito test onErrorMap MovieReactiveService

---
+++ src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceMockTest.java
@@ -0,0 +1,76 @@
+package com.learnreactiveprogramming.service;
+
+import com.learnreactiveprogramming.domain.Movie;
+import com.learnreactiveprogramming.domain.MovieInfo;
+import com.learnreactiveprogramming.exception.MovieException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class MovieReactiveServiceMockTest {
+
+    private AutoCloseable mocks;
+
+    @Mock
+    MovieInfoService movieInfoService;
+
+    @Mock
+    ReviewService reviewService;
+
+    @InjectMocks
+    MovieReactiveService movieReactiveService;
+
+//    @BeforeEach  statt  @ExtendWith(MockitoExtension.class)
+//    void setUp() {
+//        mocks = MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @AfterEach
+//    void tearDown() throws Exception {
+//        mocks.close();
+//    }
+
+    @Test
+    void getAllMovies() {
+        //given
+        var movie = new MovieInfo(98L, "Batman Begins", 2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux()).thenCallRealMethod();
+        Mockito.when(reviewService.retrieveReviewsFlux(Mockito.anyLong())).thenCallRealMethod();
+        Flux<Movie> allMovies = movieReactiveService.getAllMovies().log();
+
+        //then
+        StepVerifier.create(allMovies)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void getAllMovies_whenExceptionThrown() {
+        //given
+        var movie = new MovieInfo(98L, "Batman Begins", 2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux()).thenReturn(Flux.just(movie));
+        Mockito.when(reviewService.retrieveReviewsFlux(movie.getMovieInfoId())).thenThrow(new MovieException("TEST"));
+        Flux<Movie> allMovies = movieReactiveService.getAllMovies().log();
+
+        //then
+        StepVerifier.create(allMovies)
+                .expectError(MovieException.class)
+                .verify();
+    }
+
+
+}
