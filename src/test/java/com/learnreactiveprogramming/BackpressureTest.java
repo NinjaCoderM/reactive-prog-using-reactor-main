3879258d59055604e00e39e11852baaef66b4cbd
wienm <martin.scheibelreiter@chello.at>
2025-03-03 23:19:48 +0100

    onBackpressureDrop, difference cancel() and hookOnCancel()

--- src/test/java/com/learnreactiveprogramming/BackpressureTest.java
+++ src/test/java/com/learnreactiveprogramming/BackpressureTest.java
@@ -88,4 +88,48 @@ public class BackpressureTest {
         });
         Assertions.assertTrue(latch.await(5L, TimeUnit.SECONDS) );
     }
+    @Test
+    public void testBackpressure_drop() throws InterruptedException {
+        var numberRange = Flux.range(1, 100).log();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        //numberRange.subscribe(num -> log.info("Number: {}", num));
+        numberRange.onBackpressureDrop(item -> {
+                    log.info("Dropped items: {}", item);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(2);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                //super.hookOnNext(value);
+                log.info("hookOnNext: {}", value);
+//                if(value%2==0) request(2);
+//                else if (value > 50) cancel(); //Without dropped items in logs
+                if(value==2) hookOnCancel(); //Dropped items are in logs
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                //super.hookOnComplete();
+
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                //super.hookOnError(throwable);
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                //super.hookOnCancel();
+                log.info("hookOnCancel");
+                latch.countDown();
+            }
+        });
+        Assertions.assertTrue(latch.await(5L, TimeUnit.SECONDS) );
+    }
 }
