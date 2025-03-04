c1c97b4b3f27a7738f4156b099e34127950ef186
wienm <martin.scheibelreiter@chello.at>
2025-03-04 13:16:59 +0100

    Cold Streams

--- src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
+++ src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
@@ -1,4 +1,22 @@
 package com.learnreactiveprogramming;
 
+import jdk.dynalink.linker.ConversionComparator;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
 public class ColdAndHotPublisherTest {
+   @Test
+   public void coldPublisherTest() {
+       //given
+       var flux = Flux.range(1,10);
+       //when
+       flux.subscribe(i -> System.out.println("Subscriber1: " + i));
+       flux.subscribe(i -> System.out.println("Subscriber2: " + i));
+       //then
+
+   }
+
 }
